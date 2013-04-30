a4251bd43aac6567096817b23467078aea1c4bd9
Andrew McCloud <andrew@amccloud.com>
2013-04-29 18:36:27 -0700

    Cleaned up tabs.

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,18 +1,18 @@
 include_recipe 'glusterfs::base'
 
 package 'glusterfs-client' do
-	action :install
+  action :install
 end
 
 node[:glusterfs][:client][:mounts].each do |volume, path|
-	directory path do
-		action :create
-		recursive true
+  directory path do
+    action :create
+    recursive true
   end
 
-	mount path do
+  mount path do
     action [:mount, :enable]
-		device volume
-		fstype 'glusterfs'
-	end
+    device volume
+    fstype 'glusterfs'
+  end
 end
--- recipes/server.rb
+++ recipes/server.rb
@@ -2,22 +2,22 @@ include_recipe 'glusterfs::base'
 include_recipe 'glusterfs::peer'
 
 node[:glusterfs][:server][:peers].each do |peer|
-	execute "gluster peer probe #{peer}" do
-		not_if "gluster peer status | grep 'Hostname: #{peer}'" 
-	end
+  execute "gluster peer probe #{peer}" do
+    not_if "gluster peer status | grep 'Hostname: #{peer}'" 
+  end
 end
 
 node[:glusterfs][:server][:volumes].each do |volume, brick|
-	stripe = "stripe #{node[:glusterfs][:server][:stripe]}" if node[:glusterfs][:server][:stripe]
-	replica = "replica #{node[:glusterfs][:server][:replica]}" if node[:glusterfs][:server][:replica]
-	transport = "transport #{node[:glusterfs][:server][:transport]}" if node[:glusterfs][:server][:transport]
-	peers = node[:glusterfs][:server][:peers].map{|peer| "#{peer}:#{brick}"}.join(' ')
+  stripe = "stripe #{node[:glusterfs][:server][:stripe]}" if node[:glusterfs][:server][:stripe]
+  replica = "replica #{node[:glusterfs][:server][:replica]}" if node[:glusterfs][:server][:replica]
+  transport = "transport #{node[:glusterfs][:server][:transport]}" if node[:glusterfs][:server][:transport]
+  peers = node[:glusterfs][:server][:peers].map{|peer| "#{peer}:#{brick}"}.join(' ')
 
-	execute "gluster volume create #{volume} #{stripe} #{replica} #{transport} #{peers}" do
-		not_if "gluster volume info | grep -c 'Volume Name: #{volume}'"
-	end
+  execute "gluster volume create #{volume} #{stripe} #{replica} #{transport} #{peers}" do
+    not_if "gluster volume info | grep -c 'Volume Name: #{volume}'"
+  end
 
-	execute "gluster volume start #{volume}" do
-		not_if "gluster volume info #{volume} | grep 'Status: Started'"
-	end
+  execute "gluster volume start #{volume}" do
+    not_if "gluster volume info #{volume} | grep 'Status: Started'"
+  end
 end
